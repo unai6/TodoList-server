086bf383a0c61d066c08c07838a7721a995c78c7
unai gonzalez <unaigoe91@gmail.com>
2020-09-14 14:38:54 +0200

    token

--- appControllers/userController.js
+++ appControllers/userController.js
@@ -6,67 +6,71 @@ const { signToken } = require('../helpers/signToken');
 
 exports.userSignup = async (req, res) => {
 
-    try{
-        const {name, nickName, password} = req.body;
+    try {
+        const { name, nickName, password } = req.body;
 
-            const nickNameExists = await User.findOne({nickName}, 'nickName');
-            const salt = bcrypt.genSaltSync(saltRounds);
-            const hashPass = bcrypt.hashSync(password, salt);
-          
-            if(nickNameExists) {
-                res.status(401).send('User already exists');
-            } else {
-                const newUser = await User.create({name, nickName, password:hashPass});
-                 res.status(200).json(newUser)
-            };
-    } catch(error) {
-        res.status(400).json({error:error});
+        const nickNameExists = await User.findOne({ nickName }, 'nickName');
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hashPass = bcrypt.hashSync(password, salt);
+
+        if (nickNameExists) {
+            res.status(401).send('User already exists');
+        } else {
+            const newUser = await User.create({ name, nickName, password: hashPass });
+            res.status(200).json(newUser)
+        };
+    } catch (error) {
+        res.status(400).json({ error: error });
     }
-  
+
 }
 
 
 exports.login = async (req, res) => {
-        const { nickName, password, remember } = req.body;
-      
-        try {
-          let user = await User.findOne({nickName});
-          if(!user) return res.status(404).json({msg: 'User not found'});
-      
-          const passCorrect = bcrypt.compareSync(password, user.password);
-          if(!passCorrect) return res.status(401).json({msg: 'nickName or password not valid' });
-        
-          res.cookie(process.env.PUBLIC_DOMAIN || process.env.PUBLIC_DOMAIN, {
-            maxAge: 432000000,
-            httpOnly: true,
-            sameSite: 'None',
-            secure: true,
-          })
-            .status(200)
-         
+    const { nickName, password, remember } = req.body;
+
+    try {
+        let user = await User.findOne({ nickName });
+        if (!user) return res.status(404).json({ msg: 'User not found' });
+
+        const passCorrect = bcrypt.compareSync(password, user.password);
+        if (!passCorrect) {
+
+            return res.status(401).json({ msg: 'nickName or password not valid' })
+
+        } else {
+            res.cookie(process.env.PUBLIC_DOMAIN || process.env.PUBLIC_DOMAIN, {
+                maxAge: 432000000,
+                httpOnly: true,
+                sameSite: 'None',
+                secure: true,
+            });
             const token = signToken(user, remember);
 
-          res.status(200).json({
-            token,
-            user: {
-              userId: user.id,
-              nickName: user.nickName
-            }
-          })
-        } catch (error) {
-            console.log(error);
+            res.status(200).json({
+                token,
+                user: {
+                    userId: user.id,
+                    nickName: user.nickName
+                }
+            })
         }
-      };
 
 
-exports.logout = async  (req, res) => {
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+exports.logout = async (req, res) => {
     try {
         res.clearCookie(process.env.PUBLIC_DOMAIN);
         res.status(200).json({ msg: "Log out sucesfully" });
-      } catch (e) {
+    } catch (e) {
         console.error(e);
         res.status(500).json({ msg: "Server error" });
-      }
+    }
     return;
 }
 
@@ -76,12 +80,12 @@ exports.dashboard = async (req, res) => {
     try {
         const { userId } = req.params;
         const user = await User.findById(userId).populate({
-            path:'tasks',
-            model:'Task'
+            path: 'tasks',
+            model: 'Task'
         });
         res.status(200).json(user)
-     
-    } catch (error) { 
+
+    } catch (error) {
         console.log(error)
     }
 };
