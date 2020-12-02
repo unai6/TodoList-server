4d6798bd8f9e52af23c46baf6b9198b568163861
unai gonzalez <unaigoe91@gmail.com>
2020-12-02 11:10:44 +0100

    endpoints handlers

--- routes/todo-list.js
+++ routes/todo-list.js
@@ -1,24 +1,22 @@
 const express = require('express');
 const router = express.Router()
 
-const authController = require('../appControllers/userController');
+const {login, getUserData, getUserTasks, dashboard, signUpWithGoogle, userSignup} = require('../appControllers/userController');
+const {createTask, deleteTask, editTask} = require('../appControllers/taskController');
 const {checkToken}  = require('../helpers/middlewares');
 
-
-const tasks = require('../appControllers/taskController');
-
 /*User Routes */
-router.post('/google-signup', authController.signUpWithGoogle)
-router.post('/signup', authController.userSignup);
-router.post('/login', authController.login);
-router.get('/dashboard/:userId', checkToken, authController.dashboard)
-router.get('/userinfo/:userId', authController.getUserData)
-router.get('/:userId/alltasks', authController.getUserTasks)
+router.post('/google-signup', signUpWithGoogle)
+router.post('/signup', userSignup);
+router.post('/login', login);
+router.get('/dashboard/:userId', checkToken, dashboard)
+router.get('/userinfo/:userId', getUserData)
+router.get('/:userId/alltasks', getUserTasks)
 
 /* Tasks Routes */
 
-router.post('/create-task/:userId', tasks.createTask)
-router.post('/delete-task/:userId/:taskId', tasks.deleteTask)
-router.post('/edit-task/:userId/:taskId', tasks.editTask)
+router.post('/create-task/:userId', createTask)
+router.post('/delete-task/:userId/:taskId', deleteTask)
+router.post('/edit-task/:userId/:taskId', editTask)
 
 module.exports = router
